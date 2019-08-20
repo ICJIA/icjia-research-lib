15668afd18a2f45b9d4b5b804db6fe6b0b6f0a59
Bobae Kang <bkang.kr@gmail.com>
2019-08-20 11:07:21 -0500

    refactor: extract and make exportCSS into a file

--- build-utils/build-lib.js
+++ build-utils/build-lib.js
@@ -1,7 +1,4 @@
 /* eslint-disable no-console */
-const fs = require('fs-extra')
-const path = require('path')
-
 console.info("🚀 Let's build this thing!")
 
 // Update the index file
@@ -9,22 +6,5 @@ console.info('📝 Updating index file')
 require('./update-index-file')
 
 // Export CSS
-exportCSS()
-
-function exportCSS() {
-  const srcPath = getPath('../src/style.css')
-  const destPath = getPath('../style.css')
-
-  try {
-    if (fs.existsSync(srcPath)) {
-      fs.copyFileSync(srcPath, destPath)
-      console.info('💅 CSS file is exported.')
-    }
-  } catch (err) {
-    console.error(err)
-  }
-}
-
-function getPath(...args) {
-  return path.resolve(__dirname, ...args)
-}
+console.info('💅 Exporting CSS file')
+require('./export-css')
